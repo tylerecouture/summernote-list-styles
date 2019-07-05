d2d75073738c496e0c8230a183c2686a00ac88ba
qwe0001 <47100388+qwe0001@users.noreply.github.com>
2019-07-06 08:30:11 +0900

    bug fix that list style appending not work on IE11 (#3)
    * bug fix that list style appending not work on IE11
    
    * bug fix that list style appending not work on IE11
    
    *  bug fix that list style appending not work on IE11

--- summernote-list-styles-bs4.js
+++ summernote-list-styles-bs4.js
@@ -57,13 +57,12 @@
       var listStyleLabels = lang.listStyleTypes.labelsListStyleTypes;
 
       var list = "";
-      var index = 0;
-      for (const listStyleType of listStyleTypes) {
-        list += '<li><div class="list-style-item" data-value=' + listStyleType + ">";
+
+      for (var i = 0; i < listStyleTypes.length; i++) {
+        list += '<li><div class="list-style-item" data-value=' + listStyleTypes[i] + ">";
         list += '<i class="note-icon-menu-check float-left"></i>';
-        list += '<ol><li style="list-style-type: ' + listStyleType + ';">';
-        list += listStyleLabels[index] + "</li></ol></div></li>";
-        index++;
+        list += '<ol><li style="list-style-type: ' + listStyleTypes[i] + ';">';
+        list += listStyleLabels[i] + "</li></ol></div></li>";
       }
 
       context.memo("button.listStyles", function() {
